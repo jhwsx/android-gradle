4dcb661de0a9f887f92f09fc85621bb9aecf161f
wangzhichao <willwaywang6@gmail.com>
2022-02-08 14:54:30 +0800

    review

---
+++ chapter06/example_classpath/src/main/java/com/wzc/classpath/Main.java
@@ -0,0 +1,11 @@
+package com.wzc.classpath;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        int length = StringUtils.length("helloworld");
+        System.out.println(length);
+
+    }
+}
